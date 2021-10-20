133387d8a22d98bb6aa4fcdf7042e60e17f396f7
Alaa Asfour <alaa94@my.yorku.ca>
2021-10-20 11:05:47 -0400

    User Authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,63 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { signout } from './actions/userActions';
 import CartScreen from './Screens/CartScreen';
 import HomeScreen from './Screens/HomeScreen';
 import ProductScreen from './Screens/ProductScreen';
+import RegisterScreen from './Screens/RegisterScreen';
+import SigninScreen from './Screens/SigninScreen';
 function App() {
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
-
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
   return (
     <BrowserRouter>
-    <div className="grid-container">
-            <header className="row">
-                <div>
-                    <Link className="brand" to="/">Ararat</Link>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link className="brand" to="/">Ararat</Link>
+          </div>
+          <div>
+            <Link to="/cart">Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {
+              userInfo ? (
+                <div className="dropdown">
+                  <Link to="#">{userInfo.name} <i className="fa fa-cart-down"></i></Link>
+                  <ul className="dropdown-content">
+                    <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
+
+                  </ul>
                 </div>
-                <div>
-                    <Link to="/cart">Cart
-                    {cartItems.length > 0 && (
-                      <span className="badge">{cartItems.length}</span>
-                    )}
-                    </Link>
-                    <Link to="/signin">Sign In</Link>
-                </div>
-            </header>
-            <main>
-              <Route path="/cart/:id?" component={CartScreen}></Route>
-              <Route path="/product/:id" component={ProductScreen}></Route>
-              <Route path="/" component={HomeScreen} exact></Route>
-                
-            </main>
-            <footer className="row center"> 
-                All Right Reserved!
-            </footer>
-        </div>
-        </BrowserRouter>
+              ) :
+                (
+                  <Link to="/signin">Sign In</Link>
+                )
+            }
+
+          </div>
+        </header>
+        <main>
+          <Route path="/cart/:id?" component={CartScreen}></Route>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">
+          All Right Reserved!
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
